88efac875f5a8c54cc692567c25136fe0a538f6d
Brian Griffin <bgtalkingdog@gmail.com>
2020-09-12 09:29:27 -0500

    default routes changed from welcome to livescan

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/mdm-crt/';
+$config['base_url'] = 'https://mdm-crt.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------

537f5e4bd6726135073051b0fc60de26dd981e40
Brian Griffin <bgtalkingdog@gmail.com>
2020-09-19 16:06:40 -0500

    added image changed railroad

--- application/views/past7days.php
+++ application/views/past7days.php
@@ -7,8 +7,9 @@
 </ul>
 <h1><?php echo $subtitle; ?></h1>
 <h3><?php echo $range; ?></h3>
-<p>This log shows the time each vessel passed the rail road drawbridge at Clinton, Iowa, Lock and Dam 13 at Fulton, Illinois and whether it was traveling upriver or downriver.</p>
+<p>This log shows the time each vessel passed the railroad drawbridge at Clinton, Iowa, Lock and Dam 13 at Fulton, Illinois and whether it was traveling upriver or downriver.</p>
 <table>
   <tr><th>Name</th><th>Type</th><th>Direction</th><th>Lock 13</th><th>RR Bridge</th><th></th></tr>
   <?php echo $table; ?>
 </table>
+<img src="../images/drawbridge.jpg" height="200" alt="Clinton railroad drawbridge" />

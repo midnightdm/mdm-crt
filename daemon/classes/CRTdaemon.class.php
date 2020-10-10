83c6641342c65c2ebe3b1f80d05b8abc403ee3ea
Brian Griffin <bgtalkingdog@gmail.com>
2020-10-10 10:41:05 -0500

    fixed wrong var name

--- daemon/classes/CRTdaemon.class.php
+++ daemon/classes/CRTdaemon.class.php
@@ -162,7 +162,7 @@ class CRTdaemon  {
     $now = time();
     foreach($this->liveScan as $key => $obj) {     
       //If record is old...
-      echo 'Vessel '.$obj->liveName.' last updated '.$obj->liveLastTS . '. It\'s now '.$ts.' Timeout is '.$this->timeout.'.  ';
+      echo 'Vessel '.$obj->liveName.' last updated '.$obj->liveLastTS . '. It\'s now '.$now.' Timeout is '.$this->timeout.'.  ';
       if(($now - $this->timeout) > $obj->liveLastTS) {
         //...then save it to passages table
         if($obj->savePassageIfComplete(true)) {          

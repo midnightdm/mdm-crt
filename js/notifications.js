847ececa976ba6bed8f23384452528763ff11b61
Brian Griffin <bgtalkingdog@gmail.com>
2021-03-14 20:50:49 -0500

    Updating pusher client

--- js/notifications.js
+++ js/notifications.js
@@ -1,6 +1,6 @@
 
 const beamsClient = new PusherPushNotifications.Client({
-instanceId: 'c229f02c-e435-41ed-899f-756bf916f1f5',
+instanceId: '1ba67efe-2cfc-4fc6-af88-890780051fe8',
 });
 
 beamsClient.start()
@@ -14,6 +14,11 @@ beamsClient.start()
 function requestPassengerNotifications() {
     console.log("requestPassengerNotifications()");
     beamsClient.start()
+    .then(() => beamsClient.addDeviceInterest('CRT Passenger Vessels'))
+    .then(() => console.log('Successfully registered and subscribed!'))
+    .catch(console.error);
+    /*
+    beamsClient.start()
     .then(beamsClient => beamsClient.getDeviceId())
     .then(deviceId =>
       console.log('Successfully registered with Beams. Device ID:', deviceId)
@@ -22,6 +27,7 @@ function requestPassengerNotifications() {
     .then(() => beamsClient.getDeviceInterests())
     .then(interests => console.log('Current interests:', interests))
     .catch(console.error);
+    */
 }
 
 function requestAllNotifications() {
@@ -36,3 +42,10 @@ function requestAllNotifications() {
     .then(interests => console.log('Current interests:', interests))
     .catch(console.error);
 }
+
+
+
+
+
+
+

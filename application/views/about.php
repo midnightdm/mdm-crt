cde39b63adc18e30cfa9466f5b561bf53c3637ee
Brian Griffin <bgtalkingdog@gmail.com>
2021-04-04 22:03:49 -0500

    about page update

--- application/views/about.php
+++ application/views/about.php
@@ -5,10 +5,10 @@
         <p>This website is for river fans and barge tow spotters of the Clinton, Iowa area. It works by collecting MMSI transponder data sent via a radio device on equipped vessels using a receiver radio and a computer. The information is then parsed and saved to a database for display on these pages. Presently each vessel's location is plotted on a <a href="https://maps.google.com">Google Map</a> while other information is shown along side. Anyone who likes to watch vessels can use the site to figure when one will soon be close to them.</p>
         <h2>Responsive Styling</h2>
         <p>The first iteration of responsive styling is the latest site improvement. Mobile devices can now see site details without having to resize their viewport and a hamburger menu makes buttons easier to press. Also tell us what you think of our new landing page. Brian recorded that beautiful video himself.</p>
-            <h2>Alerts Are Here</h2>
+        <h2>Alerts Are Here</h2>
         <p>Another recent feature allows users to subscribe to push notifications to receive notices when vessels are nearing Clinton for viewing. There are two groups with different expected notice frequency.  The Passenger Vessels notice stream will give you five notices for each vessel, but such vessels arrive only a few times per month. The All Vessels notice stream limits alerts on each vessel to two, but with so much tow traffic there may be a dozen per day. There is no cost for the service and it requires giving no personal data. Activate it and get more details on the Alerts page. If you accidentally choose the wrong list, delete our site from your web browser's notification's settings, then choose the correct one to reactivate.</p>
-        <h2>Other Features?</h2>
-        <p>This site will continue to improve and  grow as it gets used and more vessel passages get logged. There are still some bugs here and there, but its getting better.  We invite your comments and suggestions for improvements. Contact us at the email address below.</p>
+        <h2>Other Features</h2>
+        <p>This site will continue to improve and grow as it gets used and more vessel passages get logged. There are still some bugs here and there, but its getting better.  We invite your comments and suggestions for improvements. Contact us at the email address below.</p>
         <ul><li>clintonrivertraffic@gmail.com</li></ul>
         <h4>Brian F. & Michael W. -- Site Owners</h4>
     </div>

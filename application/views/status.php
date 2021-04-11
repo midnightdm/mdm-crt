59f1d76f785196c61c1d508ab272d663b34d0adf
Brian Griffin <bgtalkingdog@gmail.com>
2021-04-10 20:19:49 -0500

    Added div ids to status view

--- application/views/status.php
+++ application/views/status.php
@@ -1,4 +1,8 @@
-<h3><?php echo $isReachable; ?> </h3>
-<h3>Last Up: <?php echo $lastUpTS; ?> </h3>
-<h3>Last Down: <?php echo $lastDownTS; ?></h3>
-</p>
+<div id="post-menu-body">
+    <div id="content-container">
+        <h3><?php echo $isReachable; ?> </h3>
+        <h3>Last Up: <?php echo $lastUpTS; ?> </h3>
+        <h3>Last Down: <?php echo $lastDownTS; ?></h3>
+        </p>
+    </div>
+</div>

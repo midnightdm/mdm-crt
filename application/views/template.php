ef06d59c0f733518f22b980c95da662ff141083d
Brian Griffin <bgtalkingdog@gmail.com>
2021-04-14 22:28:48 -0500

    Added google ad link to template

--- application/views/template.php
+++ application/views/template.php
@@ -10,6 +10,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/base/jquery-ui.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
+  <script data-ad-client="ca-pub-6783704513864257" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
 <body>
   <div id="wrapper">

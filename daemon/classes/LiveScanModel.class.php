b56419f145606dc7402d65705cfe73e8ee405072
Brian Griffin <bgtalkingdog@gmail.com>
2021-06-09 22:48:44 -0500

    filtered data from object before db write

--- daemon/classes/LiveScanModel.class.php
+++ daemon/classes/LiveScanModel.class.php
@@ -88,16 +88,17 @@ class LiveScanModel extends Dbh {
   }
 
   public function saveRecentScan($obj) {
-    $keys = ['liveInitTS', 'liveLastTS', 'liveInitLat', 'liveInitLon', 'liveDirection', 'liveLocation', 'liveVesselID', 'liveName', 'liveLength', 'liveWidth', 'liveDraft', 'liveCallSign', 'liveSpeed', 'liveCourse', 'liveDest', 'liveIsLocal'];
+    $keys = ['liveInitTS', 'liveLastTS', 'liveInitLat', 'liveInitLon', 'liveDirection', 'liveVesselID', 'liveName', 'liveLength', 'liveWidth', 'liveDraft', 'liveCallSign', 'liveSpeed', 'liveCourse', 'liveDest', 'liveIsLocal'];
     //Put object data into array format
     $data = array();
     foreach($keys as $key) {
       $data[$key] = $obj->$key;
     }
+    $data['liveLocation'] = $obj->liveLocation->description;
     //Get liveID to update if a past record for this vessel exits
     $liveID = $this->getRecentIDFor($obj->liveVesselID);
     if($liveID) {
-      $data['liveID'] = $liveID;
+      $data['liveID'] = $liveID;  
       $this->updateRecentScan($data);
       return null;
     } else {
